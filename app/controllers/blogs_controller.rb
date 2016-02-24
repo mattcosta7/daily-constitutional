2553e6a356b7f9a18d1cce454dc8ac554e24ff67
Matthew Costabile <mattcosta7@gmail.com>
2016-02-24 17:50:26 -0500

    updated blog collection methods

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,23 +6,30 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      current_user.blogs << @blog
-      begin
-        feed = Feedjira::Feed.fetch_and_parse @blog.url
-        feed.entries.first(10).each do |new_entry|
-          a = Entry.new(Blog.makeEntryHash(new_entry, @blog.id))
-          a.save
+    if !Blog.where(blog_params).first
+      @blog = Blog.new(blog_params)
+      if @blog.save
+        current_user.blogs << @blog
+        begin
+          feed = Feedjira::Feed.fetch_and_parse @blog.url
+          feed.entries.first(10).each do |new_entry|
+            a = Entry.new(Blog.makeEntryHash(new_entry, @blog.id))
+            a.save
+          end
+          redirect_to :back
+        rescue => ex
+          flash[:notice]="There was an issue with your link, dude!"
+          logger.error ex.message
+          redirect_to :back
         end
-        redirect_to :back
-      rescue => ex
-        flash[:notice]="There was an issue with your link, dude!"
-        logger.error ex.message
+      else
         redirect_to :back
       end
     else
-      redirect_to :back
+      if !current_user.blogs.where(blog_params).first
+        current_user.blogs << Blog.where(blog_params).first
+      end
+      redirect_to current_user
     end
   end
   
