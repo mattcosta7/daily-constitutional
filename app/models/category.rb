21879b01147afde4212f38f8169d14768806e249
Matthew Costabile <mattcosta7@gmail.com>
2016-02-28 22:02:21 -0500

    edits to category input styling

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :blogcategories
   has_many :blogs, through: :blogcategories
+
+  validates :title, presence:true, uniqueness:{ case_sensitive: false }
 end
