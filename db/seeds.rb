5a95a3a01095cb79c8d9420d2f0c4de70d2fd637
Matthew Costabile <mattcosta7@gmail.com>
2016-03-01 09:43:53 -0500

    seed for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,46 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# if Category.all.length == 0
-#   cats = ['Art',
-#     'Economics',
-#     'Parenting',
-#     'Career',
-#     'Finance',
-#     'Gaming',
-#     'Nature',
-#     'Wine',
-#     'Beer',
-#     'Medicine',
-#     'Sports',
-#     'Science',
-#     'Technology',
-#     'Business',
-#     'Photography',
-#     'Law',
-#     'Wedding',
-#     'Travel',
-#     'Music',
-#     'Other'
-#   ]
+if Category.all.length == 0
+  cats = ['Art',
+    'Economics',
+    'Parenting',
+    'Career',
+    'Finance',
+    'Gaming',
+    'Nature',
+    'Wine',
+    'Beer',
+    'Medicine',
+    'Sports',
+    'Science',
+    'Technology',
+    'Business',
+    'Photography',
+    'Law',
+    'Wedding',
+    'Travel',
+    'Music',
+    'Other'
+  ]
 
-#   cats.each do |title|
-#     Category.create(title: title)
-#   end
-# end
-
-# if User.all.length < 10
-#   100.times do |guy|
-#     user = User.new(email: Faker::Internet.email, password:'123456', password_digest: '123456', location: Faker::Internet.ip_v4_address)
-#     user.save
-#   end
-# end
-
-
-for i in 3...User.all.length
-  x = rand(Blog.all.length)
-  y = rand(Blog.all.length-1) + 1
-  x.times do |something|
-    User.find(i).blogs << Blog.find(y) unless User.find(i).blogs.include?(Blog.find(y))
+  cats.each do |title|
+    Category.create(title: title)
   end
 end
+
+if User.all.length < 10
+  100.times do |guy|
+    user = User.new(email: Faker::Internet.email, password:'123456', password_digest: '123456', location: Faker::Internet.ip_v4_address)
+    user.save
+  end
+end
+
+
+# for i in 3...User.all.length
+#   x = rand(Blog.all.length)
+#   y = rand(Blog.all.length-1) + 1
+#   x.times do |something|
+#     User.find(i).blogs << Blog.find(y) unless User.find(i).blogs.include?(Blog.find(y))
+#   end
+# end
